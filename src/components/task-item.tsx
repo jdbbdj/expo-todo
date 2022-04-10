136a7df72df11c21f0436b84e7a779caf640f797
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-04-10 20:31:13 +0800

    Fix some issues from last try

---
+++ src/components/task-item.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const task-item = () => {
+  return (
+    <View>
+      <Text>task-item</Text>
+    </View>
+  )
+}
+
+export default task-item
