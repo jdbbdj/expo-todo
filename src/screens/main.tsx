b0a2636eaa2b7355355effcbce110a4852317c92
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2022-04-18 06:15:52 +0800

    Main Screen Refactor, Animate Task List, Configure data manipulation and add Add Task functionality

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -5,20 +5,91 @@ import {
   Box,
   Center,
   VStack,
-  themeTools,
-  useTheme,
-  useColorMode,
+  Fab,
+  Icon,
   useColorModeValue
 } from 'native-base'
 import ThemeToggle from '../components/theme-toggle'
 import TaskItem from '../components/task-item'
 import TaskList from '../components/task-list'
+import shortid from 'shortid'
+
+const initialData = [
+  {
+    id: shortid.generate(),
+    subject: 'Buy movie tickets for Friday',
+    done: false
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Buy movie tickets for Sat',
+    done: false
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Buy movie tickets for Sunday',
+    done: false
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Buy movie tickets for Monday',
+    done: false
+  },
+  {
+    id: shortid.generate(),
+    subject: 'Buy movie tickets for Tuesday',
+    done: false
+  }
+]
+
+const PlusIcon = () => {
+  return (
+    <Text color="white" fontSize="15px">
+      +
+    </Text>
+  )
+}
+
 export default function MainScreen() {
-  const [checked, setChecked] = useState(false)
-  const [subject, setSubject] = useState('Task Item')
-  const [isEditing, setIsEditing] = useState(true)
-  const handlePressCheckbox = useCallback(() => {
-    setChecked(prev => !prev)
+  const [data, setData] = useState(initialData)
+  const [editingItemId, setEditingItemId] = useState<string | null>(null)
+  const handleToggleTaskItem = useCallback(item => {
+    setData(prevData => {
+      const newData = [...prevData]
+      const index = prevData.indexOf(item)
+      newData[index] = {
+        ...item,
+        done: !item.done
+      }
+      return newData
+    })
+  }, [])
+
+  const handleChangeTaskItemSubject = useCallback((item, newSubject) => {
+    setData(prevData => {
+      const newData = [...prevData]
+      const index = prevData.indexOf(item)
+      newData[index] = {
+        ...item,
+        subject: newSubject
+      }
+      return newData
+    })
+  }, [])
+
+  const handleFinishEditingTaskItem = useCallback(_item => {
+    setEditingItemId(null)
+  }, [])
+
+  const handlePressTaskItemLabel = useCallback(item => {
+    setEditingItemId(item.id)
+  }, [])
+
+  const handleRemoveItem = useCallback(item => {
+    setData(prevData => {
+      const newData = prevData.filter(i => i !== item)
+      return newData
+    })
   }, [])
 
   return (
@@ -28,22 +99,37 @@ export default function MainScreen() {
       flex={1}
     >
       <VStack space={5} alignItems="center" w="full">
-        <TaskItem
-          isDone={checked}
-          onToggleCheckbox={handlePressCheckbox}
-          subject={subject}
-          onChangeSubject={setSubject}
-          onPressLabel={() => {
-            setIsEditing(true)
-          }}
-          isEditing={isEditing}
-          onFinishedEditing={() => {
-            setIsEditing(false)
-          }}
+        <TaskList
+          data={data}
+          onToggleItem={handleToggleTaskItem}
+          onChangeSubject={handleChangeTaskItemSubject}
+          onFinishEditing={handleFinishEditingTaskItem}
+          onRemoveItem={handleRemoveItem}
+          onPressLabel={handlePressTaskItemLabel}
+          editingItemId={editingItemId}
         />
         <ThemeToggle />
-        <TaskList />
       </VStack>
+      <Fab
+        position="absolute"
+        renderInPortal={false}
+        size="sm"
+        icon={<Icon as={<PlusIcon />} />}
+        colorScheme={useColorModeValue('blue', 'darkBlue')}
+        bg={useColorModeValue('blue.500', 'blue.400')}
+        onPress={() => {
+          const id = shortid.generate()
+          setData([
+            {
+              id,
+              subject: '',
+              done: false
+            },
+            ...data
+          ])
+          setEditingItemId(id)
+        }}
+      />
     </Center>
   )
 }
